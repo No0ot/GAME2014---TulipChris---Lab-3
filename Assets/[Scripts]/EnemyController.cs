575122e5cc3a8d7df29dd410d375ea925956d462
Chris <tulip.chris@gmail.com>
2021-10-07 15:27:48 -0400

    Enemy and Enemy bullets added

---
+++ Assets/[Scripts]/EnemyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Enemy Movement")]
+    public Bounds moveBounds;
+    public Bounds startRange;
+
+    [Header("Bullets")]
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public int frameDelay;
+
+    public float startingPoint;
+    public float randomSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpeed = Random.Range(moveBounds.min, moveBounds.max);
+        startingPoint = Random.Range(startRange.min, startRange.max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(Mathf.PingPong(Time.time, randomSpeed) + startingPoint, transform.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        if(Time.frameCount % frameDelay == 0)
+        {
+            var tempBullet = Instantiate(bulletPrefab);
+
+            tempBullet.transform.position = bulletSpawn.position;
+        }
+    }
+}
